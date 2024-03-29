b09b51e5cd1ec0e7d1a2ab2462ec44be56f60d17
Yaric <yaosamo@gmail.com>
2024-03-29 09:35:11 -0700

    hello world number 2

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,55 @@
-
 module Main exposing(..)
-import Html exposing (..)
+import Browser
+import Html exposing (Html, div, button, text)
+import Html.Events exposing (..)
 
 greet : String -> String
 greet name = 
       name ++ " welcome!"
 
-main =
-      text (greet "yaric")
+main = 
+      Browser.sandbox { init = init, update = update, view = view }
+
+-- Model
+
+type alias Model = Float
+
+init : Model
+init = 0
+
+-- Update
+
+type Message =
+      Increment | Double
+      
+
+update : Message -> Model -> Model
+update msg model = 
+      case msg of
+      Increment ->
+            model + 1
+      Double ->
+            model * 2
+
+-- If statement
+
+checkNumber : Float -> String
+checkNumber num =
+    if num <= 25.0 then
+        "Not enough"
+    else
+        "Oh that's too much!"
+
+
+-- View
+
+view : Model -> Html Message
+view counter = 
+      div []
+      [ 
+            button [] [ text "Down" ],
+      div [] [ text (String.fromFloat counter)],
+            button [ onClick Increment ] [ text "Up" ],
+             button [ onClick Double ] [ text "Double" ],
+      div [] [ text (checkNumber counter) ]
+      ]
